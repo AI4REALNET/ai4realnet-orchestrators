4317e97beed0840194277953f924eb80d3df92e3
ahmouckeri <erick.ah-mouck@artelys.com>
2025-11-24 14:46:38 +0000

    Fix division by zero error on operation score

--- ai4realnet_orchestrators/power_grid/test_runner_kpi_of_036_power_grid.py
+++ ai4realnet_orchestrators/power_grid/test_runner_kpi_of_036_power_grid.py
@@ -9,13 +9,15 @@ class TestRunner_KPI_OF_036_Power_Grid(PowerGridTestRunner):
     kpi = OperationScoreKpi()
     kpi_result = kpi.evaluate(record_directory)
     n_redispatch = kpi_result[1]
-    e_redipatch = kpi_result[2]
+    e_redispatch = kpi_result[2]
     e_balancing = kpi_result[3]
     n_curtailment = kpi_result[4]
     e_curtailment = kpi_result[5]
     e_losses = kpi_result[6]
     e_blackout = kpi_result[7]
-    primary_kpi_value = (e_redipatch / n_redispatch) + e_balancing + (e_curtailment / n_curtailment) + e_losses + e_blackout
+    redispatch_fraction = e_redispatch / n_redispatch if n_redispatch !=0 else 0
+    curtailment_fraction = e_curtailment / n_curtailment if n_curtailment !=0 else 0
+    primary_kpi_value = redispatch_fraction + e_balancing + curtailment_fraction + e_losses + e_blackout
 
     return {
       "primary": primary_kpi_value
